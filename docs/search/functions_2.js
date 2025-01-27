dbee3c62bc49d149aaee331d522ed5c866416004
Enrico Marchionni <enrico.marchionni@studio.unibo.it>
2025-01-27 17:24:17 +0100

    Update OpenMP implementation: update initialization of sin() and cos() precalculated tables, update doxygen documentation

--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['inittables_0',['initTables',['../omp-projection_8c.html#a5000146a352c8329bb90f666a9cc7e77',1,'omp-projection.c']]]
+  ['inittables_0',['initTables',['../omp-projection_8c.html#af0d19fe4c3150a7feb2c29081941a6fc',1,'omp-projection.c']]]
 ];
